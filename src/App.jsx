47b85bc31c356db6a7794afee7db960310e4c507
Rencas <renzo.c@ohecommerce.agency>
2023-04-08 12:27:00 -0500

    clima-app initialization project + first components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import AppClima from './components/AppClima'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <AppClima />
+  )
+}
+
+export default App
---
+++ src/components/AppClima.jsx
@@ -0,0 +1,10 @@
+
+export default function AppClima() {
+   return (
+      <>
+         <main className="dos-columnas">
+            <Formulario />
+         </main>
+      </>
+   )
+}
